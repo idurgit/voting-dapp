daaf346b9ee1a8ea77be3271f0a1b61c95112e3e
idurgit <cakidurwsl@Armaggeddon.>
2024-12-10 09:08:08 +0700

    Initial commit

---
+++ programs/voting/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod voting {
+    use super::*;
+
+    pub fn initialize_poll(ctx: Context<InitializePoll>, _poll_id: u64) -> ProgramResult {
+      Ok(())
+    }
+
+  }
